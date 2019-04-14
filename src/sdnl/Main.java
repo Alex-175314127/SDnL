c0be7eaaae283b7e0f6bc5aa0b28b108ff333e5c
Alexander Adam <Alexander Adam@192.168.1.1>
2019-04-14 12:14:36 +0700

    versi x8.21

--- src/sdnl/Main.java
+++ src/sdnl/Main.java
@@ -6,6 +6,7 @@
 package sdnl;
 
 import java.awt.Image;
+import java.util.Scanner;
 import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 
@@ -19,9 +20,10 @@ public class Main {
      * @param args the command line arguments
      * @throws java.lang.Exception
      */
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         // TODO code application logic here
-        Tree t =new Tree();
+        Tree t = new Tree();
+        Scanner s = new Scanner(System.in);
         t.insert(42);
         t.insert(21);
         t.insert(38);
@@ -34,24 +36,48 @@ public class Main {
         t.insert(2);
         t.insert(40);
         t.insert(12);
-        
-        int key = 38;
-        TreeNode found; 
+
+        System.out.print(" preOrder : ");
+        t.preOrderTraversal();
+        System.out.print("\n inOreder : ");
+        t.inOrderTraversal();
+        System.out.print("\n postOrder : ");
+        t.postOrderTraversal();
+
+        System.out.print("\n depth : ");
+        t.depth(7);
+        System.out.print(" height : ");
+        t.height(7);
+        System.out.println("");
+        System.out.print("cari data : ");
+
+        int key = s.nextInt();
+        TreeNode found;
         found = t.Search(key);
         if (found != null) {
-            System.out.println("Data "+key+" Found in BST");
-        }else{
-            System.out.println("Data "+key+" not Found");
+            System.out.println("Data " + key + " Found in BST");
+        } else {
+            System.out.println("Data " + key + " not Found");
         }
-        
-        System.out.print(" preOrder : ");t.preOrderTraversal();
-        System.out.print("\n inOreder : ");t.inOrderTraversal();
-        System.out.print("\n postOrder : ");t.postOrderTraversal();
-        System.out.println("");
-        System.out.print("depth : ");t.depth(40);
-        System.out.print("height : ");t.height(40);
-        
-        
+
+        System.out.print("Hapus data : ");
+        int key2 = s.nextInt();
+        if (t.Delete(key2) != null) {
+            t.Delete(key2);
+            System.out.println("DATA SETELAH MENGHAPUS NILAI " + key + ":");
+            System.out.print("preOrder  : ");
+            t.preOrderTraversal();
+            System.out.println();
+            System.out.print("inOrder   : ");
+            t.inOrderTraversal();
+            System.out.println();
+            System.out.print("postOrder : ");
+            t.postOrderTraversal();
+            System.out.println();
+        } else {
+            System.out.println("Data Tidak ketemu!");
+        }
+
 //        ImageIcon icon = new ImageIcon("b.png");
 //        ImageIcon icon2 = new ImageIcon("1.png");
 //        Image image = icon.getImage();
@@ -82,8 +108,6 @@ public class Main {
 //        }else{
 //          JOptionPane.showMessageDialog(null, e, "Error", JOptionPane.PLAIN_MESSAGE, icon);
 //            throw  e;
-           
-        
     }
-    
+
 }
